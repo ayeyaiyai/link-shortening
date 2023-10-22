2450128305f0bcbba3a6fce93c661d2ea0bfa917
ayeyaiyai <105176518+ayeyaiyai@users.noreply.github.com>
2023-10-22 12:28:07 -0400

    Import Statistics.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Header from './components/Header.jsx';
 import GetStarted from './components/GetStarted.jsx';
+import Statistics from './components/Statistics.jsx';
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <div className='shortly-container'>
       <Header />
       <GetStarted />
+      <Statistics />
     </div>
   )
 }
