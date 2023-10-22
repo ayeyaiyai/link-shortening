f64d474d592dc8bb05e6d6d1a3f43db32291ae49
ayeyaiyai <105176518+ayeyaiyai@users.noreply.github.com>
2023-10-22 11:51:35 -0400

    Add GetStarted elements (no functionality yet)

---
+++ src/components/GetStarted.jsx
@@ -0,0 +1,29 @@
+import '../styles/GetStarted.css';
+import working from '../images/illustration-working.svg';
+
+function GetStarted() {
+    return (
+        <div className="get-started-container">
+            <div className='get-started-body'>
+                <div className='get-started-left'>
+                    <div className='get-started-title'>More than just shorter links</div>
+                    <div className='get-started-subtitle'>Build your brand's recognition and get detailed insights on how your links are performing.</div>
+                    <button className='get-started-button'>Get Started</button>
+                </div>
+                <div className='get-started-right'><img src={working} id='working-image' alt='worker working illustration'/></div>
+            </div>
+            <div className='link-shorten-body'>
+                <div className='input-section'>
+                    <input 
+                    id='shorten-input'
+                    placeholder='Shorten a link here...'
+                    >
+                    </input>
+                    <button className='shorten-button'>Shorten It!</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default GetStarted;
