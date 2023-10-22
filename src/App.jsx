e6be4f777bdfa1c923b867cbf35516c91d643f44
ayeyaiyai <105176518+ayeyaiyai@users.noreply.github.com>
2023-10-22 11:51:09 -0400

    Import GetStarted.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './components/Header.jsx';
+import GetStarted from './components/GetStarted.jsx';
 
 function App() {
 
   return (
     <div className='shortly-container'>
       <Header />
+      <GetStarted />
     </div>
   )
 }
