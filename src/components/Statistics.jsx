bd9b1f8ae8915297252843b74748eae6c01a9244
ayeyaiyai <105176518+ayeyaiyai@users.noreply.github.com>
2023-10-22 13:33:21 -0400

    Add title/subtitle elements

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -3,6 +3,8 @@ import '../styles/Statistics.css';
 function Statistics() {
     return(
         <div className='statistics-container'>
+            <div className='statistics-title'>Advanced Statistics</div>
+            <div className='statistics-subtitle'>Track how your links are performing across the web with our advanced statistics dashboard.</div>
         </div>
     )
 }
