830d2c9c3023ef193adaf938e00c1d12c82911fb
ayeyaiyai <105176518+ayeyaiyai@users.noreply.github.com>
2023-10-23 11:42:11 -0400

    Add LinkBoost component elements

---
+++ src/components/LinkBoost.jsx
@@ -0,0 +1,12 @@
+import '../styles/LinkBoost.css';
+
+function LinkBoost() {
+    return (
+        <div className="link-boost-container">
+            <div className='link-boost-title'>Boost your links today</div>
+            <button className='get-started-button'>Get Started</button>
+        </div>
+    )
+}
+
+export default LinkBoost;
