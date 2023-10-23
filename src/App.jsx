7651ef28996257e1e77d5e19a305657d1330eb63
ayeyaiyai <105176518+ayeyaiyai@users.noreply.github.com>
2023-10-23 12:37:03 -0400

    Import Links.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header.jsx';
 import GetStarted from './components/GetStarted.jsx';
 import Statistics from './components/Statistics.jsx';
 import LinkBoost from './components/LinkBoost.jsx';
+import Links from './components/Links';
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       <GetStarted />
       <Statistics />
       <LinkBoost />
+      <Links />
     </div>
   )
 }
